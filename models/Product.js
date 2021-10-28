57e3f823ff16f0a97eb23a15ebbe5e3dab648a06
Travis Puryear <travispuryear678@gmail.com>
2021-10-28 16:12:43 -0400

    columns defined for Product.

--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,41 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+
+    },
+    product_name: {
+      type : DataTypes.STRING,
+      allowNull: false
+      
+    },
+    price : {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+
+    },
+    stock: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: 5,
+      validate: {
+        isNumeric: true,
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model : 'category_id',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
--- models/index.js
+++ models/index.js
@@ -14,18 +14,20 @@ Category.hasMany(Product, {
   foreignKey: "category_id",
 });
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(
-  Tag,
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id'
+})
 
-  { through: { ProductTag } }
-);
+  
+
 
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(
-  Product,
+Tag.belongsToMany(Product, {
 
-  { through: ProductTag }
-);
+  through: ProductTag,
+  foreignKey: 'tag_id'
+});
 
 module.exports = {
   Product,
