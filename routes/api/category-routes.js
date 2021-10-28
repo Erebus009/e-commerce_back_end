bd0b6e83fd3f6a671d25ee7c8d546af8d195ddab
Travis Puryear <travispuryear678@gmail.com>
2021-10-28 17:45:10 -0400

    Category delete method added.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -40,12 +40,30 @@ router.post("/", async (req, res) => {
   }
 })  
 
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a category by its `id` value
+  try {
+    const updateCategory = await Category.update({category_name: req.body.category_name},{where:{ id:req.params.id}});
+    if(!updateCategory[0]) {
+      res.status(404).json({message : "No Categories match that Id| Or names match"})
+    }
+    res.status(200).json(updateCategory)
+  } catch(err){
+    res.status(500).json(err)
+  }
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete a category by its `id` value
-});
+  try {
+    const destroyCategory = await Category.destroy({where:{id:req.params.id}});
+    if(!destroyCategory) {
+      res.status(404).json({message: "No Category macthes that ID"})
+      }
+    res.status(200).json(destroyCategory)
+  } catch (err){
+    res.status(500).json(err)
+  }
+})
 
 module.exports = router;
