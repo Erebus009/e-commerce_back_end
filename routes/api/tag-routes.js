51b2f161812f3e3bc8deee1134d31d3cedfdf108
Travis Puryear <travispuryear678@gmail.com>
2021-10-28 20:05:12 -0400

    TAG put and delete METHOD coded in.

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,39 +1,79 @@
-const router = require('express').Router();
-const { Tag, Product, ProductTag } = require('../../models');
+const router = require("express").Router();
+const { Tag, Product, ProductTag } = require("../../models");
 
 // The `/api/tags` endpoint
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   // find all tags
-  const tags = await Tag.findAll({include: {model: Product}}).catch((err) => {
-    res.status(500).json(err)
-  })
-  res.status(200).json(tags)
-  // be sure to include its associated Product data
+  const allTags = await Tag.findAll({
+    include: {
+      model: Product,
+      attributes: ["product_name", "price", "category_id", "stock"],
+    },
+  }).catch((err) => {
+    res.json(err);
+  });
+  res.status(200).json(allTags);
 });
+//------------------------------
+// Find a single tag by its `id`|
+//------------------------------
 
-router.get('/:id', async (req, res) => {
-  // find a single tag by its `id`
-  const singleTag = await Tag.findOne({where:{id: req.params.id}}, {include: {model: Product}}).catch((err) => {
-    res.status(500).json(err)
-  })
-  if(!singleTag){
-    res.json({message:"No tag associatedwith this ID"})
+router.get("/:id", async (req, res) => {
+  const singleTag = await Tag.findOne({
+    where: { id: req.params.id },
+    include: {
+      model: Product,
+      attributes: ["product_name", "price", "category_id", "stock"],
+    },
+  }).catch((err) => {
+    res.status(500).json(err);
+  });
+  if (!singleTag) {
+    res.json({ message: "No tag associated with this ID" });
   }
-  res.status(200).json(singleTag)
-  // be sure to include its associated Product data
+  res.status(200).json(singleTag);
 });
+//=====================================================
+// POST a single tag that meets the req.body conditions
+//======================================================
 
-router.post('/', (req, res) => {
+
+router.post("/", async (req, res) => {
   // create a new tag
+  const createTag = await Tag.create({ tag_name: req.body.tag_name }).catch(
+    (err) => {
+      res.status(500).json(err);
+    }
+  );
+  res.status(200).json(createTag);
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a tag's name by its `id` value
-});
+  const updateTag = await Tag.update(
+    { tag_name: req.body.tag_name },
+    { where: { id: req.params.id } }
+  ).catch((err) => {
+    res.status(500).json(err);
+  });
 
-router.delete('/:id', (req, res) => {
+  res.status(200).send(updateTag);
+});
+//==================================
+// Deletes a single tag based on it's tag id 
+//=======================================
+
+
+router.delete("/:id", async (req, res) => {
   // delete on tag by its `id` value
+  const destoryTag = await Tag.destroy({where:{id : req.params.id}}).catch((err) => {
+    res.status(500).json(err)
+  })
+  if(!destoryTag){
+    res.status(404).json({message:"No Tag matches that ID"})
+  }
+  res.status(200).json(destoryTag)
 });
 
 module.exports = router;
